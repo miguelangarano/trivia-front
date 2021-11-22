ca54b72705f11155c19bd50b34f8d5cab7232ed1
Miguel Langarano <miguelangarano@gmail.com>
2021-11-22 15:24:55 -0500

    environment

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -2,54 +2,55 @@ import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { HttpClient } from '@angular/common/http';
 import { ApiResponse } from './api-response.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GameService {
-  
-  currentQuestion:string="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis veniam a consequatur veritatis, nostrum tenetur, minus, soluta fuga ex laborum magni id. Quaerat libero rem ab, hic consequatur eos!"
-  answerOptions: string[]=[
-    "opcion1",
-    "opcion2",
-    "opcion3",
-    "opcion4"
-  ]
-  correctAnswer:string="opcion3"
-  userService:UserService;
-  httpClient:HttpClient
-  constructor(userService:UserService,httpClient:HttpClient) {
-    this.httpClient=httpClient
-    this.userService=userService
-  }
 
-  async getTriviaQuestion():Promise<ApiResponse>{
-    const data = await this.httpClient.get("http://localhost:4001/questions/single",{headers:{user:this.userService.userName,"access-token":this.userService.token}}).toPromise();
-    const response = JSON.parse(JSON.stringify(data))
-    return {status:response.status,message:response.message,data:response.data}
-  }
+    currentQuestion: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis veniam a consequatur veritatis, nostrum tenetur, minus, soluta fuga ex laborum magni id. Quaerat libero rem ab, hic consequatur eos!"
+    answerOptions: string[] = [
+        "opcion1",
+        "opcion2",
+        "opcion3",
+        "opcion4"
+    ]
+    correctAnswer: string = "opcion3"
+    userService: UserService;
+    httpClient: HttpClient
+    constructor(userService: UserService, httpClient: HttpClient) {
+        this.httpClient = httpClient
+        this.userService = userService
+    }
 
-  async setNewTriviaQuestion(){
-    const response = await this.getTriviaQuestion();
-    console.log(response)
-    this.currentQuestion = response.data.question.question
-    this.answerOptions=response.data.question.answer
-    this.correctAnswer=response.data.question.correctAnswer
-  }
+    async getTriviaQuestion(): Promise<ApiResponse> {
+        const data = await this.httpClient.get(environment.serverUrl + "/questions/single", { headers: { user: this.userService.userName, "access-token": this.userService.token } }).toPromise();
+        const response = JSON.parse(JSON.stringify(data))
+        return { status: response.status, message: response.message, data: response.data }
+    }
 
-  sendUserResponse(question:string,answer:string){
-    this.httpClient.post(
-      "http://localhost:4001/questions/response",
-      {
-        question:question,
-        answer:answer
-      },
-      {
-        headers:{
-          user:this.userService.userName,
-          "access-token":this.userService.token
-        }
-      }
-    ).toPromise();
-  }
+    async setNewTriviaQuestion() {
+        const response = await this.getTriviaQuestion();
+        console.log(response)
+        this.currentQuestion = response.data.question.question
+        this.answerOptions = response.data.question.answer
+        this.correctAnswer = response.data.question.correctAnswer
+    }
+
+    sendUserResponse(question: string, answer: string) {
+        this.httpClient.post(
+            environment.serverUrl + "/questions/response",
+            {
+                question: question,
+                answer: answer
+            },
+            {
+                headers: {
+                    user: this.userService.userName,
+                    "access-token": this.userService.token
+                }
+            }
+        ).toPromise();
+    }
 }
--- src/app/positions.service.ts
+++ src/app/positions.service.ts
@@ -2,37 +2,39 @@ import { Injectable } from '@angular/core';
 import { ApiResponse } from './api-response.model';
 import { Gamer } from './gamer.model';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PositionsService {
-  httpClient:HttpClient
-  constructor(httpClient:HttpClient) {
-    this.httpClient=httpClient
-  }
-
-  gamersPosition: Gamer[]=[
-    new Gamer(1,"Alejandro",2),
-    new Gamer(2,"Alejandro",2),
-    new Gamer(2,"Alejandro",2),
-    new Gamer(2,"Alejandro",2)
-  ]
-
-  async getScoreBoard():Promise<ApiResponse>{
-    try{
-    const data = await this.httpClient.get("http://localhost:4001/game/scoreboard").toPromise();
-    const response = JSON.parse(JSON.stringify(data))
-    console.log(response.data.player[0])
-    return {status:response.status,message:response.message,data:response.data}
-    }catch(error){
-      console.log(error)
+    httpClient: HttpClient
+    constructor(httpClient: HttpClient) {
+        this.httpClient = httpClient
     }
-  }
-  
-  async setPositions(){
-    const response = await this.getScoreBoard()
-    this.gamersPosition=response.data.player
-    console.log(response)
-  }
+
+    gamersPosition: Gamer[] = [
+        new Gamer(1, "Alejandro", 2),
+        new Gamer(2, "Alejandro", 2),
+        new Gamer(2, "Alejandro", 2),
+        new Gamer(2, "Alejandro", 2)
+    ]
+
+    async getScoreBoard(): Promise<ApiResponse> {
+        try {
+            const data = await this.httpClient.get(environment.serverUrl + "/game/scoreboard").toPromise();
+            const response = JSON.parse(JSON.stringify(data))
+            console.log(response.data.player[0])
+            return { status: response.status, message: response.message, data: response.data }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async setPositions() {
+        const response = await this.getScoreBoard()
+        this.gamersPosition = response.data.player
+        console.log(response)
+    }
 }
 
